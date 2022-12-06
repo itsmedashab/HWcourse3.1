5f26950914face015e1ff35b48aa899f5bbc7a37
Dasha <dashabutenko@list.ru>
2022-12-06 23:41:46 +0400

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+int index = 1;
+int a;
+
+Console.Write($"Enter your number: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+while (index < a)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
+    index++;
+}
