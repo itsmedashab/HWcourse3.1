8b1fe1c0e847661285c775ac78d5809fd48538d4
Dasha <dashabutenko@list.ru>
2022-12-06 23:32:20 +0400

    add task3

--- task2/Program.cs
+++ task2/Program.cs
@@ -16,5 +16,4 @@ int max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
-Console.Write("max = ");
-Console.WriteLine(max);
+Console.Write($"max = {max}\n");
---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+int a;
+
+Console.Write($"Enter your number: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Your number is even");
+}
+else
+{
+   Console.WriteLine($"Your number is odd"); 
+}
