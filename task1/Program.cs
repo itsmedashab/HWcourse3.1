873152df136ea6ba9b9bb2c8ba394f0564d43fdb
Dasha <dashabutenko@list.ru>
2022-12-06 23:11:02 +0400

    add task 1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+int a;
+int b;
+
+Console.Write($"Enter first number: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Enter second number: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+if (a > b)
+{
+    Console.WriteLine($"First number is bigger than second");
+}
+else
+{
+    Console.WriteLine($"Second number is bigger than first");
+}
